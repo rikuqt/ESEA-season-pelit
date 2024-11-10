f9f87a61ef7617ac64692211484c84284f860615
rikuqt <riku.tomann@gmail.com>
2024-11-10 11:37:39 +0200

    tausta kuva pelistä, ajan muuttuja vaihto

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import axios from "axios"
 import './App.css'
 
   // HAE TIEDOT KOSKA PELI ALKAA
-  function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
+  function timeConverter(pelin_aika){
+    var a = new Date(pelin_aika * 1000);
     var months = ['Tammikuu','Helmikuu','Maaliskuu','Huhtikuu','Toukokuu','Kesäkuu','Heinäkuu','Elokuu','Syyskuu','Lokakuu','Marraskuu','Joulukuu'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
@@ -16,42 +16,31 @@ import './App.css'
     return time;
   }
 
-  // Haku pelin mapin kuvalle
-  function mapinKuva () {
-    let mapinKuva_id = ""
-    matchroom.voting.map.entities.forEach(mappi => {
-      if (mappi.game_map_id == matchroom.voting.map.pick[0]){
-        mapinKuva_id = mappi.image_lg
-      }
-    })
-  return mapinKuva_id
-}
-
-const UpcomingGame = ({mapin_kuva, competition, fc1_avatar, fc2_avatar, fc1_name, fc2_name, aika}) => {
-  return <div style={{backgroundImage: `url(${mapin_kuva})`}} className="center">
-    <h2>Tuleva peli: </h2>
-    <h1>{competition}</h1>
-    <p className="jeejee"><img src={fc1_avatar} className="Logo" /> {fc1_name} vs {fc2_name} <img src={fc2_avatar} className="Logo" /></p>
-    <p>Pelin ajoitus: {timeConverter(aika)}</p>
-    </div>
-}
-
-const GameDefault = ({competition, score, fc1_avatar, fc2_avatar, fc1_name, fc2_name, fc1_id, kaitonki_id, winner_id}) => {
-  let winner
-  if (!fc1_id==kaitonki_id && !kaitonki_id==winner_id) {
-    winner=fc1_name
-  } else {
-    winner=fc2_name
+  const UpcomingGame = ({competition, fc1_avatar, fc2_avatar, fc1_name, fc2_name, aika}) => {
+    return <div className="center">
+      <h2>Tuleva peli: </h2>
+      <h1>{competition}</h1>
+      <p className="jeejee"><img src={fc1_avatar} className="Logo" /> {fc1_name} vs {fc2_name} <img src={fc2_avatar} className="Logo" /></p>
+      <p>Pelin ajoitus: {timeConverter(aika)}</p>
+      </div>
   }
 
-  return <div className="center">
-    <h2>Viimeisin peli: </h2>
-    <h1>{competition}</h1>
-    <p className="jeejee">{score}</p> <p className="jeejee"><img src={fc1_avatar} className="Logo" /> {fc1_name} vs {fc2_name} <img src={fc2_avatar} className="Logo" /></p>
-    <p>Voittaja: {winner} <form action="https://stackoverflow.com/questions/2906582/how-do-i-create-an-html-button-that-acts-like-a-link"><input type="submit" value="Lataa demo"/></form>
-    </p>
-    </div>
-}
+  const GameDefault = ({mapinKuva_osoite, competition, score, fc1_avatar, fc2_avatar, fc1_name, fc2_name, fc1_id, kaitonki_id, winner_id}) => {
+    let winner
+    if (fc1_id!==kaitonki_id && kaitonki_id!==winner_id) {
+      winner=fc1_name
+    } else {
+      winner=fc2_name
+    }
+
+    return <div  style={{backgroundImage: `url(${mapinKuva_osoite})`}} className="center">
+      <h2>Viimeisin peli: </h2>
+      <h1>{competition}</h1>
+      <p className="jeejee">{score}</p> <p className="jeejee"><img src={fc1_avatar} className="Logo" /> {fc1_name} vs {fc2_name} <img src={fc2_avatar} className="Logo" /></p>
+      <p>Voittaja: {winner} <form action="https://stackoverflow.com/questions/2906582/how-do-i-create-an-html-button-that-acts-like-a-link"><input type="submit" value="Lataa demo"/></form>
+      </p>
+      </div>
+  }
 
 
 function App() {
@@ -59,15 +48,12 @@ function App() {
     const [stats ,setStats] = useState([])
     const [upComingMatch ,setUpComingMatch] = useState([])
     const [loading, setLoading] = useState(true);
-    const tulevaPeli_aika = 1731006000
-
-
-  
+    
 
     // Api key + 
     const api_key = "3c7c45ce-12d8-4f53-af38-95b64a714dd5"
-    const game_id = "1-d9823f78-a875-4470-9837-d42609fa3cdd"
-    const upcoming_game_id = "1-5cb0778d-8900-478a-b250-a49a1af261af"
+    const game_id = "1-5cb0778d-8900-478a-b250-a49a1af261af"
+    const upcoming_game_id = "1-71628d2c-011c-42ee-8199-80e20ad7e2fc"
     const matchStats_url = `https://open.faceit.com/data/v4/matches/${game_id}/stats`
     const matchroom_url = `https://open.faceit.com/data/v4/matches/${game_id}`
     const up_coming_matchroom_url = `https://open.faceit.com/data/v4/matches/${upcoming_game_id}`
@@ -88,10 +74,10 @@ function App() {
         .then(axios.spread((responseOne, responseTwo, responseThree) => {
           setMatchroom(responseOne.data),
           setStats(responseTwo.data),
-          setUpComingMatch(responseThree.data)
+          setUpComingMatch(responseThree.data),
           console.log(responseOne.data),
           console.log(responseThree.data),
-          console.log("kaikki data haettu")
+          console.log("kaikki data haettu"),
           setLoading(false)
         }))
       }
@@ -120,19 +106,33 @@ function App() {
         },
         competition_name: upComingGame_competition_type
       } = upComingMatch;
+
+
       // Tarvittavat muuttujat
       const roundScore = stats.rounds[0].round_stats.Score
       const kaitonki_id = "1ce16320-21c5-4cfe-a4e1-1fcb599a2a35"
-      
+      const tulevaPeli_aika = upComingMatch.scheduled_at
       // Asyncistä ei saa otettua tämän tietoja -> joku ratkaisu axiokseen ettei
       // tule virheitä siitä ettei dataa ole vielä saatu 
       // mapinKuva={mapinKuva()} 
+
+      
+  // Haku pelin mapin kuvalle + ei toimi app:n ulkopuolella -> async kusee
+      function MapinKuva () {
+        let mapinKuva_id = "" // muuta tätä jälkeenpäin
+        matchroom.voting.map.entities.forEach(mappi => {
+          if (mappi.game_map_id == matchroom.voting.map.pick[0]){
+            mapinKuva_id = mappi.image_lg
+          } 
+        })
+      return mapinKuva_id
+    }
 
   return (
     <div>
     <body>
       <UpcomingGame competition={upComingGame_competition_type} fc1_avatar={upComingGame_fc1_avatar} fc2_avatar={upComingGame_fc2_avatar} fc1_name={upComingGame_fc1_name} fc2_name={upComingGame_fc2_name} aika={tulevaPeli_aika}/>
-      <GameDefault competition={LastGame_competition_type} score={roundScore} fc1_avatar={LastGame_fc1_avatar} fc2_avatar={LastGame_fc2_avatar}
+      <GameDefault mapinKuva_osoite={MapinKuva()} competition={LastGame_competition_type} score={roundScore} fc1_avatar={LastGame_fc1_avatar} fc2_avatar={LastGame_fc2_avatar}
       fc1_name={LastGame_fc1_name} fc2_name={LastGame_fc2_name} fc1_id={LastGame_fc1_id} kaitonki_id={kaitonki_id} winner_id={LastGame_winner_id}/>   
       </body>
     </div>
